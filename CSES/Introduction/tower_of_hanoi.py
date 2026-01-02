7d443bcc694778bace92232d380172532ba2a761
Luciano 16-gif <chanominardo10@gmail.com>
2026-01-01 21:29:32 -0400

    forgot to remove a placeholder comment

--- CSES/Introduction/tower_of_hanoi.py
+++ CSES/Introduction/tower_of_hanoi.py
@@ -2,7 +2,7 @@ def main() -> None:
     n: int = int(input())
     min_moves: int = 2**n - 1
     print(min_moves)
-    def solve(num_of_disks, src, aux, dest): #this is just a simulation not the real code
+    def solve(num_of_disks, src, aux, dest): 
         if num_of_disks == 1:
             print(src, dest)
             return
