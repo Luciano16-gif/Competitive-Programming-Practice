2e0fecf52b97ed3c0834857978e3db3efcca1f2a
Luciano 16-gif <chanominardo10@gmail.com>
2026-01-04 22:34:39 -0400

    a friend bullied me into making better variable names

--- CSES/Introduction/chessboard_and_queens.py
+++ CSES/Introduction/chessboard_and_queens.py
@@ -1,26 +1,26 @@
 def main() -> None:
     board = [input().strip() for _ in range(8)]
-    col: list[bool] = [False] * 8
-    diag1: list[bool] = [False] * 15
-    diag2: list[bool] = [False] * 15
+    cols: list[bool] = [False] * 8
+    diagonal1: list[bool] = [False] * 15
+    diagonal2: list[bool] = [False] * 15
     ans: int = 0
 
-    def mark_dismark(r, c, val) -> None:
-        col[c] = val
-        diag1[r - c + 7] = val
-        diag2[r + c] = val
+    def mark_dismark(row, col, val) -> None:
+        cols[col] = val
+        diagonal1[row - col + 7] = val
+        diagonal2[row + col] = val
 
-    def dfs(r):
+    def dfs(row):
         nonlocal ans
-        if r == 8:
+        if row == 8:
             ans += 1
             return
 
-        for c in range(8):
-            if board[r][c] != "*" and not col[c] and not diag1[r - c + 7] and not diag2[r + c]:
-                mark_dismark(r, c, True)
-                dfs(r+1)
-                mark_dismark(r, c, False)
+        for col in range(8):
+            if board[row][col] != "*" and not cols[col] and not diagonal1[row - col + 7] and not diagonal2[row + col]:
+                mark_dismark(row, col, True)
+                dfs(row+1)
+                mark_dismark(row, col, False)
 
     dfs(0)
     print(ans)
